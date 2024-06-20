3498cb97b9fdc0a3a1f3a6dfe9dfa94f4462d109
Bassam Data <105807570+bassamsdata@users.noreply.github.com>
2024-06-20 11:44:02 -0400

    feat: refinement of logging messages and copy mechanism

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -11,12 +12,7 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
-// TODO: add this to the readme,
-// config file should look like this
-
-// TODO: add comments to the code
-//TODO: add logic to truncate the log file with no dependency before getting so big
-// TODO: move the logging logic to a separate function
+// TODO: add logic to truncate the log file with no dependency before getting so big
 
 // [folders]
 //     [folders.folder1]
@@ -29,8 +25,9 @@ import (
 
 // Config structure
 type FolderConfig struct {
-	Source      string   `toml:"source"`
-	Destination []string `toml:"destination"`
+	OriginalSource string   `toml:"originalSource"`
+	Source         string   `toml:"source"`
+	Destination    []string `toml:"destination"`
 }
 
 type Config struct {
@@ -38,10 +35,9 @@ type Config struct {
 }
 
 func main() {
-
 	// Create a log file
 	logpath := filepath.Join(os.Getenv("HOME"), "repos", "syncAuto", "syncAuto.log")
-	logFile, err := os.OpenFile(logpath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logFile, err := os.OpenFile(logpath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
 	if err != nil {
 		fmt.Println("Error creating log file:", err)
 		return
@@ -56,7 +52,6 @@ func main() {
 	}
 
 	var config Config
-	// TODO: change it once creating the init function
 	configFilePath := filepath.Join(os.Getenv("HOME"), ".config", "syncAuto", "config.toml")
 
 	if _, err := toml.DecodeFile(configFilePath, &config); err != nil {
@@ -72,6 +67,11 @@ func main() {
 			continue
 		}
 
+		copyFrom, _ := expandTilde(folder.OriginalSource)
+		if err := copyItem(copyFrom, expandedSource, logger); err != nil {
+			logger.Printf("Warning copying item: '%s', maybe the original source path is empty: '%s'", err, copyFrom)
+		}
+
 		for _, dest := range folder.Destination {
 			destParts := strings.Split(dest, ":")
 			if len(destParts) != 2 {
@@ -84,10 +84,88 @@ func main() {
 			syncToRemote(expandedSource, remoteType, remotePath, logger)
 		}
 	}
-
 }
 
-// TODO: add function to copy the file to the destination, so it's gonna by full automated service
+// copyItem determines if the source is a file or directory and calls the appropriate function
+func copyItem(src, dst string, logger *log.Logger) error {
+	info, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		return copyDirectory(src, dst, logger)
+	}
+	dst = filepath.Join(dst, filepath.Base(src)) // Add the file name to the destination path
+	return copyFile(src, dst, logger)
+}
+
+// copyDirectory copies a directory from src to dst
+func copyDirectory(src, dst string, logger *log.Logger) error {
+	// Create the destination directory if it doesn't exist
+	if err := os.MkdirAll(dst, 0o755); err != nil {
+		return err
+	}
+
+	// List all files and directories within src
+	entries, err := os.ReadDir(src)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		srcEntry := filepath.Join(src, entry.Name())
+		dstEntry := filepath.Join(dst, entry.Name())
+
+		if entry.IsDir() {
+			// Recursively copy subdirectories
+			if err := copyDirectory(srcEntry, dstEntry, logger); err != nil {
+				return err
+			}
+			// TODO: remove this
+		} else {
+			// Copy files
+			if err := copyFile(srcEntry, dstEntry, logger); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func copyFile(srcPath, dstPath string, logger *log.Logger) error {
+	// 1. Open the source file for reading
+	srcFile, err := os.Open(srcPath)
+	if err != nil {
+		logger.Println("Error opening source file: ", err)
+		return fmt.Errorf("error opening source file: %s", err)
+	}
+	defer srcFile.Close()
+
+	// 2. Create destination file
+	dstFile, err := os.Create(dstPath)
+	if err != nil {
+		logger.Println("Error creating destination file: ", err)
+		return fmt.Errorf("error creating destination file: %s", err)
+	}
+	defer dstFile.Close()
+
+	// 3. Copy the contents
+	if _, err = io.Copy(dstFile, srcFile); err != nil {
+		logger.Println("Error copying file contents: ", err)
+		return fmt.Errorf("error copying file contents: %s", err)
+	}
+
+	// 4. Ensure contents are written to disk
+	err = dstFile.Sync()
+	if err != nil {
+		logger.Println("Error syncing destination file: ", err)
+		return fmt.Errorf("error syncing destination file: %s", err)
+	}
+
+	logger.Println("File copied successfully!")
+	return nil
+}
 
 func syncToRemote(folder, sourceRemote, destRemote string, logger *log.Logger) {
 	_, err := exec.LookPath("/usr/local/bin/rclone")
@@ -101,7 +179,7 @@ func syncToRemote(folder, sourceRemote, destRemote string, logger *log.Logger) {
 	if err != nil {
 		logger.Printf("Note syncing folder '%s' to '%s': %v\nOutput: %s\n", folder, destRemote, err, out)
 	} else {
-		logger.Printf("Folder '%s' synced successfully to '%s'\n", folder, destRemote)
+		logger.Printf("Folder '%s' synced successfully to '%s:%s'\n", folder, sourceRemote, destRemote)
 	}
 }
 
@@ -128,7 +206,7 @@ func createConfigFile(logger *log.Logger) error {
 	configFile := filepath.Join(configDir, "config.toml")
 
 	// Check if the config file already exists
-	if _, err := os.Stat(configFile); err == nil {
+	if _, err = os.Stat(configFile); err == nil {
 		logger.Println("Note Config file already exists at:", configFile)
 		return nil
 	} else if !os.IsNotExist(err) {
@@ -136,7 +214,7 @@ func createConfigFile(logger *log.Logger) error {
 		return err
 	}
 
-	err = os.MkdirAll(configDir, 0755)
+	err = os.MkdirAll(configDir, 0o755)
 	if err != nil {
 		logger.Printf("error creating config directory: %v", err)
 		return err
